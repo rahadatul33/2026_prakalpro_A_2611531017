4a734f2297a50357660e52cf44d01e49a0ec2aba
rahadatul33 <rahadatultetriana@gmail.com>
2026-09-06 13:06:34 +0700

    LAPRAK1

---
+++ progam1.py
@@ -0,0 +1,12 @@
+#Progam pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Progam ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\ ")
+print( "progam ini" + "dibuat oleh rahadatul" + "rahadatul aisha tetriana putri dan 2611531017.")
